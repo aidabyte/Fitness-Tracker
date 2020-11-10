36bda2940f4b6cb56fb4625ce251dc7c38b8c947
aidabyte <ablinstrubyte@gmail.com>
2020-11-10 17:28:28 -0600

    changed database name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/fathomless-savannah-49970',
+    process.env.MONGODB_URI || 'mongodb://localhost/tracker',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
